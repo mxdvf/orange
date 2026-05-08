81f318d9cdf1e8e514e4f8dd82e5c351e54ecd04
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2026-05-08 23:25:35 +0530

    chore: switching to offset based linear search

--- btree/btree.go
+++ btree/btree.go
@@ -1,6 +1,7 @@
 package btree
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
@@ -11,21 +12,21 @@ const (
 )
 
 const (
-	HEADER_SIZE = 4  // 4 bytes
-	PTR_SIZE    = 2  // 2 bytes
-	KEY_SIZE    = 8  // 8 bytes
-	VALUE_SIZE  = 16 // 16 bytes
+	HEADER_SIZE  = 4 // 2 + 2 = 4 bytes
+	PTR_SIZE     = 4 // 4 bytes
+	OFFSET_SIZE  = 2 // 2 bytes
+	KEY_LEN_SIZE = 2 // 2 bytes
+	VAL_LEN_SIZE = 2 // 2 bytes
 )
 
 type Node struct {
+	// wire format:
+	// type  |  nkeys |   pointers  |  offset-array	 |		     key-values
+	//  2B   |   2B   |  nkeys * 4B |  	nkeys * 2B	 |  [klen: 2B][k][vlen: 2B][v]
 	data []byte
-	//  type  |  nkeys |   pointers  |  key-values
-	//   2B   |   2B   |  nkeys * 2B |  nkeys * 24B
 }
 
 // TODO:
-// 0. fuck the book's design -- it's genuinely rubbish
-// 1. keep this node in memory but interact with bytes -- do not think of persistence yet
 // 3. come up with code that can ONLY READ FOR NOW the pointers and key-value pairs
 // 4. forget everything and only perform operations assume only this node exists, remove/insert keys, etc.
 
@@ -48,26 +49,46 @@ func newNode(t int, nkeys uint16) *Node {
 
 // Only for internal debugging
 func (n *Node) debugPrint() {
-	fmt.Println("only showing top 10 bytes:")
-	fmt.Println(n.data[:10])
+	fmt.Println("only showing top 100 bytes:")
+	fmt.Println(n.data[:100])
 }
 
-func (n *Node) nodeType() uint16 {
-	return binary.BigEndian.Uint16(n.data[0:])
+func (n *Node) getType() int {
+	return int(binary.BigEndian.Uint16(n.data[0:]))
 }
 
-func (n *Node) nkeys() uint16 {
-	return binary.BigEndian.Uint16(n.data[2:])
+func (n *Node) getNKeys() int {
+	return int(binary.BigEndian.Uint16(n.data[2:]))
 }
 
-func (n *Node) getPtr(idx int) uint16 {
-	offset := HEADER_SIZE + idx*PTR_SIZE
-	return binary.BigEndian.Uint16(n.data[offset:])
+func (n *Node) getAppropriateIdx(target []byte) (int, int) {
+	// TODO: switch to binary search, right now
+	// this is standard linear search
+	start := HEADER_SIZE + n.getNKeys()*PTR_SIZE
+	var offsetPos, nextOffsetPos uint16
+	for i := 0; i < n.getNKeys(); i++ {
+		offsetPos = binary.BigEndian.Uint16(n.data[start+(i*OFFSET_SIZE):])
+
+		key := n.getKeyByOffset(offsetPos)
+		if res := bytes.Compare(target, key); res == -1 || res == 0 {
+			nextOffsetPos = uint16(start + ((i + 1) * OFFSET_SIZE))
+			break
+		}
+	}
+	return int(offsetPos), int(nextOffsetPos)
 }
 
-func (n *Node) setPtr(idx int, v uint16) {
-	offset := HEADER_SIZE + idx*PTR_SIZE
-	binary.BigEndian.PutUint16(n.data[offset:], v)
+func (n *Node) getKeyByOffset(offset uint16) []byte {
+	keyLen := binary.BigEndian.Uint16(n.data[offset:])
+
+	start := offset + KEY_LEN_SIZE
+	end := offset + KEY_LEN_SIZE + keyLen
+
+	return n.data[start : end+1]
 }
 
-// TODO: linear search for the keys
+// TODO: we're assuming the key will fit into the node
+// TODO: we're assuming the key to be inserted is unique
+// TODO: we're assuming the key is not large enough to destroy a node (let's say node is empty and it occupies all the space)
+func (n *Node) insertKvPair(k, v []byte) {
+}
--- btree/btree_test.go
+++ btree/btree_test.go
@@ -1,12 +1,12 @@
 package btree
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestSimple(t *testing.T) {
 	n := NewLeafNode(2)
+	n.insertKvPair([]byte("你"), []byte("mehul"))
+	n.insertKvPair([]byte("kacky"), []byte("mehul"))
 	n.debugPrint()
-	fmt.Println(n.nodeType())
 }
